d4751bbd9caecb1281bf816996a3101efb40956c
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-08-21 12:09:06 +0200

    kreirane rute za users u kontroleru

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get() // GET /users
+    findAll(@Query('role') role?: 'OFFICE_WORKER' | 'PHYSICAL_WORKER') {
+        return []
+    }
+
+    @Get(':id') // GET /users/:id
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
+
+    @Post() // POST /users
+    create(@Body() userCreateDto: { name: string, email: string, role: string }) {
+        return userCreateDto
+    }
+
+    @Patch(':id') // PATCH /users/:id
+    update(@Param('id') id: string, @Body() userUpdateDto: { name?: string, email?: string, role?: string }) {
+        return userUpdateDto
+    }
+
+    @Delete(':id') // DELETE /users/:id
+    remove(@Param('id') id: string) {
+        return { id }
+    }
+
+}
