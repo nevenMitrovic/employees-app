9564169745d10a7a383c17c53492d388f7db38b5
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-08-29 11:35:27 +0200

    dizajn login forme, notfound page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NotFound from "./pages/NotFound"
 
 function App() {
 
   return (
-    <>
-      <div>START FRONT DEV</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+
+const Dashboard = () => {
+  return (
+    <>
+    <h1 className="font-bold text-4xl">EMPLOYEE APP</h1>
+    <p>DASHBOARD</p>
+    </>
+  )
+}
+
+export default Dashboard
