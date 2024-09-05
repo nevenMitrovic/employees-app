8c9b03f5177092a6c3ff87bcaac7cfac11f3b9b1
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2024-09-05 16:23:59 +0200

    zavrsena post forma

--- frontend/src/components/forms/Button.jsx
+++ frontend/src/components/forms/Button.jsx
@@ -1,9 +1,9 @@
 
-const Button = ({ text, color, hoverColor, textSize, buttonType, func }) => {
+const Button = ({ text, color, hoverColor, textSize, buttonType, func, width = 'w-[70px]' }) => {
     return (
         <button
             type={buttonType}
-            className={`text-white ${color} hover:${hoverColor} font-medium rounded-lg ${textSize} px-2 py-2`}
+            className={`text-white ${color} hover:${hoverColor} font-medium rounded-lg ${textSize} px-2 py-2 ${width}`}
             onClick={func}
         >
             { text }
--- frontend/src/components/forms/Post.jsx
+++ frontend/src/components/forms/Post.jsx
@@ -1,11 +1,32 @@
+import { useNavigate } from 'react-router-dom'
+import Button from '../forms/Button'
+import { ArrowLeft } from 'lucide-react'
 
 const Post = () => {
-    return ( 
-        <div className="bg-gray-50 px-6 py-8 mx-auto w-full max-h-screen h-[97.4vh]">
+    const navigate = useNavigate()
+
+    return (
+        <div className="bg-gray-50 px-6 py-8 w-full max-h-screen h-[97.4vh] relative">
+            <ArrowLeft
+                size={32}
+                className="absolute left-1 top-1 hover:text-gray-400 cursor-pointer"
+                onClick={() => navigate(-1)}
+            />
             <h1 className="flex items-center justify-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 Create New User
             </h1>
-            <div className="bg-white w-full py-4 px-2 rounded-md">
+            <div className="bg-white w-full py-4 px-2">
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="John Doe"
+                    required
+                />
+            </div>
+            <div className="bg-white w-full py-4 px-2">
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                 <input
                     type="email"
@@ -14,6 +35,91 @@ const Post = () => {
                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
                     required
+                />
+            </div>
+            <div className="bg-white w-full py-4 px-2">
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input
+                    type="text"
+                    name="password"
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="*******"
+                    required
+                />
+            </div>
+            <div className="bg-white w-full py-4 px-2">
+                <label htmlFor="role" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                <select id="role" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected>Choose User role</option>
+                    <option value="0">Admin</option>
+                    <option value="1">Office worker</option>
+                    <option value="2">Physical worker</option>
+                </select>
+            </div>
+            <div className="bg-white w-full py-4 px-2">
+                <label htmlFor="experience" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Experience</label>
+                <input
+                    type="number"
+                    name="experience"
+                    id="experience"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="5"
+                />
+            </div>
+            <div className="bg-white w-full py-4 px-2">
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Benefits</label>
+                <div class="flex items-center mb-4">
+                    <input id="answ1" type="checkbox" value="0" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label for="answ1" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Yes</label>
+                </div>
+                <div class="flex items-center">
+                    <input id="answ2" type="checkbox" value="1" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label for="answ2" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">No</label>
+                </div>
+            </div>
+            <div className="bg-white w-full py-4 px-2">
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Drink</label>
+                <div class="flex items-center mb-4">
+                    <input id="answ1" type="checkbox" value="0" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label for="answ1" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Yes</label>
+                </div>
+                <div class="flex items-center">
+                    <input id="answ2" type="checkbox" value="1" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label for="answ2" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">No</label>
+                </div>
+            </div>
+            <div className="bg-white w-full py-4 px-2">
+                <label htmlFor="experience" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Coefficient</label>
+                <input
+                    type="number"
+                    name="coefficient"
+                    id="coefficient"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="1.61"
+                />
+            </div>
+            <div className="bg-white w-full py-4 px-2">
+                <label htmlFor="experience" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Started</label>
+                <input type="date" name="date-picker" id="date-picker" className="border p-2 border-gray-300 rounded-md" />
+            </div>
+            <div className="bg-white w-full py-4 px-2">
+                <label htmlFor="perHour" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Per hour RSD</label>
+                <input
+                    type="number"
+                    name="perHour"
+                    id="perHour"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="160"
+                />
+            </div>
+            <div className="bg-white w-full py-4 px-2">
+                <Button
+                    text={'Save'}
+                    color={'bg-blue-600'}
+                    hoverColor={'bg-blue-300'}
+                    textSize={'text-sm'}
+                    buttonType={'button'}
                 />
             </div>
         </div>
